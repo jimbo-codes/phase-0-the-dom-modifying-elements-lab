88ba8a670b0f7b6b709a218face0e3846132647d
James <jrburns96@aol.com>
2021-10-18 14:13:16 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'James is the champion';
+document.body.append(newHeader);
+
+/*
+const hBody = document.createElement("p");
+newheader.textContent = 'James is the champion';
+//document.head.newHeader.append(hBody)
+victory.appendChild(hBody); */
